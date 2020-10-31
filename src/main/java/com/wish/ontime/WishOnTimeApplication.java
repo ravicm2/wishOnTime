ee00d0f62d4d9f1901ca5dd7429dad644e66b19d
Ravichandran Muthukumar <ashwin.muthukumar@genesys.com>
2020-10-31 14:35:43 +0530

    project base structure

---
+++ src/main/java/com/wish/ontime/WishOnTimeApplication.java
@@ -0,0 +1,13 @@
+package com.wish.ontime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishOnTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WishOnTimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wish/ontime/WishOnTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wish.ontime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishOnTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
