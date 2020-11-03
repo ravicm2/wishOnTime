525e5b996971a74b4204cd2548b13be78279bf5e
Ravichandran Muthukumar <ashwin.muthukumar@genesys.com>
2020-11-03 22:22:09 +0530

    added repos for entities

--- src/main/java/com/wish/ontime/WishOnTimeApplication.java
+++ src/main/java/com/wish/ontime/WishOnTimeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WishOnTimeApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(WishOnTimeApplication.class, args);
 	}
 
